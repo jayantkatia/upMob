87cb7dcafe52f68006c0662c1336cad28e1669b8
Jayant Katia <jayantkatia65@gmail.com>
2021-12-28 17:48:35 +0530

    fix: change env variables name

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,6 +1,11 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class API {
-  String? port = dotenv.env['PORT'];
-  Uri getDevicesURI() => Uri(scheme: 'http', host: dotenv.env['HOST'], port: port != null ? int.parse(port!) : null, path: dotenv.env['PATH']);
+  String? port = dotenv.env['API_PORT'];
+  Uri getDevicesURI() => Uri(
+        scheme: 'http',
+        host: dotenv.env['API_HOST'],
+        port: port != null ? int.parse(port!) : null,
+        path: dotenv.env['API_PATH'],
+      );
 }
