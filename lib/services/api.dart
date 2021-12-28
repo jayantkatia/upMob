9a846a7ec9a53bfe8c948f2cdd5e417d98575cd0
Jayant Katia <jayantkatia65@gmail.com>
2021-12-28 19:17:34 +0530

    fix: getAPI method null safety

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,11 +1,10 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class API {
-  String? port = dotenv.env['API_PORT'];
   Uri getDevicesURI() => Uri(
         scheme: 'http',
         host: dotenv.env['API_HOST'],
-        port: port != null ? int.parse(port!) : null,
+        port: int.tryParse(dotenv.env['API_PORT']!),
         path: dotenv.env['API_PATH'],
       );
 }
